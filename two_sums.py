d1ad87d4bce85de7188d532eefcfa5663de2b5df
ChurchillV <vinchurch70@gmail.com>
2023-06-03 19:09:28 +0000

    In case no values are found

--- two_sums.py
+++ two_sums.py
@@ -9,14 +9,17 @@ You can return the answer in any order.
 def two_sums(target, nums):
     index1 = 0
     index2 = 0
-    for i in range(0, len(nums)-1):
+    for i in range(0, len(nums)):
         for j in range(i, len(nums)):
             if nums[i] + nums[j] == target and i != j:
                 index1 = i
                 index2 = j
-    answers = (index1, index2)
-    return answers
+                print( f"Elements summing up to {target} can be found at indexes: {index1}, {index2}")
+                return
+            elif i >= len(nums)-1 and index2 == 0:
+                print("No values available")
 
 
 test_array = [2, 4, 5, 7]
-print(two_sums(9, test_array))
+target_value = 11
+two_sums(target_value, test_array)
